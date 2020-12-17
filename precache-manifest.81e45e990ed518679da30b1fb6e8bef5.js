e3e5bc84a51e8c36b2a5e907bdb16403e8a3ea0d
yarik hotchenco <yarikhot00@gmail.com>
2020-12-17 21:38:08 +0200

    Updates

---
+++ precache-manifest.81e45e990ed518679da30b1fb6e8bef5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99cf87867a5623c93490a04dc2d03f53",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2306259dec6ae671dc11",
+    "url": "./static/css/main.b368982a.chunk.css"
+  },
+  {
+    "revision": "c554a3c5b09fe87aa063",
+    "url": "./static/js/2.1d2cc816.chunk.js"
+  },
+  {
+    "revision": "4de260d6564ec0f9f9fe04c4c7432c78",
+    "url": "./static/js/2.1d2cc816.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2306259dec6ae671dc11",
+    "url": "./static/js/main.87e5162b.chunk.js"
+  },
+  {
+    "revision": "da6e1857291de4855bb602d67c39a264",
+    "url": "./static/js/main.87e5162b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29465657d3532af8e554",
+    "url": "./static/js/runtime-main.bdf20302.js"
+  },
+  {
+    "revision": "f9d3f7cea5d99c97bf6b7b5a396fb97b",
+    "url": "./static/media/avatar.f9d3f7ce.svg"
+  }
+]);
